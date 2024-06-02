719d0f09986fc457630f622e67640775858be478
Zephany <zephanilaopoku4@gmail.com>
2024-06-02 19:04:27 +0000

    added ongoingtask

---
+++ components/Ongoingtask.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default function CategoryList() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.biggerText}>Ongoing Tasks</Text>
+            <View style={styles.listContainer}>
+                {tasks.map(task => (
+                    <View style={styles.itemTaskContainer} key={task.key}>
+                        <Text style={styles.bigText}>{task.name}</Text>
+                    </View>
+                ))}
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 0,
+    },
+    listContainer: {
+        flexDirection: "column",
+        flexWrap: "nowrap",
+        justifyContent: "space-between",
+    },
+    itemTaskContainer: {
+        width: "125%",
+        marginBottom: 20,
+        padding: 25,
+        backgroundColor: "#fff",
+        borderWidth: 3,
+        borderColor: '#E8D1BA',
+        borderRadius: 20,
+    },
+    bigText: {
+        fontWeight: "bold",
+        fontSize: 15,
+    },
+    biggerText: {
+        fontWeight: "bold",
+        fontSize: 20,
+        marginBottom: 6,
+    },
+});
